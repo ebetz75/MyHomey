d3b83dc25452761ec33c1c559e780d2cf0035b98
ebetz75 <ebetz75@yahoo.com>
2025-11-20 13:27:39 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
@@ -44,6 +45,7 @@ export default defineConfig(({ mode }) => {
     },
     build: {
       outDir: 'dist',
+      sourcemap: true,
     }
   };
 });
